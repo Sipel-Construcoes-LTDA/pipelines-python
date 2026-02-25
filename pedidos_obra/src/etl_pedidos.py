6ef2e6f69758c00c9568a808c9e7be16bf448d65
ZATO <controledesempenho@sipel.com.br>
2026-02-25 09:50:14 -0300

    feat(pedidos): configuração do Etl de pedidos

--- pedidos_obra/src/etl_pedidos.py
+++ pedidos_obra/src/etl_pedidos.py
@@ -1,7 +1,5 @@
 import logging
 
-import pandas as pd
-
 # Configuração de Logging conforme PADROES_PROJETO.md
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
