4f6bd21dcc5d5cfa8cf21b17f97fa018fd1c6d6d
Thali <thalissondamiao1@gmail.com>
2026-03-02 10:01:57 -0300

    feat(reservas): .bat executavel adivionadl

---
+++ materiais_obra/src/etl_reservas_completo.bat
@@ -0,0 +1,5 @@
+@echo off
+python "C:\Users\sipel\OneDrive - sipel.com.br\pipelines-dados-SIPEL\materiais_obra\src\etl_materiais.py"
+python "C:\Users\sipel\OneDrive - sipel.com.br\pipelines-dados-SIPEL\materiais_obra\src\etl_reservas.py"
+python "C:\Users\sipel\OneDrive - sipel.com.br\pipelines-dados-SIPEL\materiais_obra\src\etl_movimentacoes_detalhada.py"
+exit
