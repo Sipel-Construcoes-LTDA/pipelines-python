97ed7fd6fdc966973d7d9e80d6054cab11dc9aa9
Thali <thalissondamiao1@gmail.com>
2026-03-31 09:34:00 -0300

    feat: atualização na base de dados e corrigido .bat

--- vistorias_comercial/src/etl_vistorias.bat
+++ vistorias_comercial/src/etl_vistorias.bat
@@ -1,3 +1,3 @@
 @echo off
-python "C:\Users\sipel\OneDrive - sipel.com.br\pipelines-dados-SIPEL\vistorias_comercial\src\etl_vistoriais.py"
+python "C:\Users\sipel\OneDrive - sipel.com.br\pipelines-dados-SIPEL\vistorias_comercial\src\etl_vistorias.py"
 exit
